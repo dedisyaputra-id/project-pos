9b8596f53971bf7d53c3a811046aefcff5846d57
Dedi Syaputra <dedisyaputra611@gmail.com>
2024-02-05 09:17:23 +0700

    crud barang selesai

--- app/Controllers/Barang.php
+++ app/Controllers/Barang.php
@@ -2,7 +2,6 @@
 
 namespace App\Controllers;
 
-
 use App\Models\BarangModel;
 
 class Barang extends BaseController
@@ -19,8 +18,8 @@ class Barang extends BaseController
     public function index(): string
     {
         session();
-        $query = "SELECT * FROM tbm_barang WHERE dlt='f'";
-        $produk = $this->db->query($query)->getResult();
+        $query = "SELECT * FROM tbm_barang WHERE dlt=? ORDER BY barangid DESC";
+        $produk = $this->db->query($query, 'f')->getResult();
         $data = [
             "title" => "Barang",
             "barang" => $produk
@@ -80,11 +79,11 @@ class Barang extends BaseController
                 ]
             ],
             "file_gambar" => [
-                "rules" => "uploaded[file_gambar]",
+                "rules" => "uploaded[file_gambar]|ext_in[file_gambar,png,jpg]",
                 "errors" => [
                     "uploaded" => "Gambar produk tidak boleh kosong",
                     "max_size" => "Ukuran gambar produk harus kurang dari 2 MB",
-                    "mime_in" => "Format gambar harus png/jpg"
+                    "ext_in" => "Format gambar harus png ataujpg"
                 ]
             ]
         ])) {
@@ -100,53 +99,156 @@ class Barang extends BaseController
             $file = $this->request->getFile("file_gambar");
             $file->move("assets/gambar-produk", $currval->currval . "." . $file->getExtension());
             $file_gambar = $file->getName();
-            $input = [
-                "file_gambar" =>  $file_gambar,
-                "barangid" => $barangId->nextval,
-                "barangname" => $this->request->getVar("barangname"),
-                "stokawal" => $this->request->getVar("stokawal"),
-                "barangcode" => $this->request->getVar("barangcode"),
-                "hargabeli" => $this->request->getVar("hargabeli"),
-                "hargajual" => $this->request->getVar("hargajual"),
-                "hargapp" => $this->request->getVar("hargapp"),
-                "remarks" => $this->request->getVar("remarks"),
-                "satuan" => $this->request->getVar("satuan"),
-                "opadd" => "admin",
-                "pcadd" => $this->request->getIPAddress(),
-                "luadd" => date("Y-m-d H:i:s"),
-            ];
-            $this->barangModel->save($input);
+
+            $gambar =  $file_gambar;
+            $barangid = $barangId->nextval;
+            $barangname = $this->request->getVar("barangname");
+            $stokawal = $this->request->getVar("stokawal");
+            $barangcode = $this->request->getVar("barangcode");
+            $hargabeli = $this->request->getVar("hargabeli");
+            $hargajual = $this->request->getVar("hargajual");
+            $hargapp = $this->request->getVar("hargapp");
+            $remarks = $this->request->getVar("remarks");
+            $satuan = $this->request->getVar("satuan");
+            $opadd = "admin";
+            $pcadd = $this->request->getIPAddress();
+            $luadd = date("Y-m-d H:i:s");
+            $inactive = $this->request->getPost('inactive');
+
+            $query = "INSERT INTO tbm_barang(barangid,file_gambar,barangname, stokawal, barangcode,hargabeli,hargajual,hargapp,remarks,satuan,opadd,pcadd,luadd,inactive) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            $this->db->query($query, [
+                $barangid, $gambar, $barangname, $stokawal, $barangcode,
+                $hargabeli, $hargajual, $hargapp, $remarks, $satuan, $opadd, $pcadd, $luadd, $inactive
+            ]);
             session()->setFlashdata("success", "Data berhasil disimpan");
             return redirect()->to("/barang");
         }
     }
-    public function detail(string $barangcode)
+    public function detail(string $barangid)
     {
-        $query = "SELECT * FROM tbm_barang WHERE barangcode = '$barangcode'";
-        $product = $this->db->query($query)->getRow();
+        $query = "SELECT * FROM tbm_barang WHERE barangid =?";
+        $product = $this->db->query($query, $barangid)->getRow();
         $data = [
             "title" => "Detail produk",
             "product" => $product
         ];
         if (empty($data["product"])) {
-            throw new \CodeIgniter\Exceptions\PageNotFoundException("title " . $barangcode . " not found");
+            throw new \CodeIgniter\Exceptions\PageNotFoundException("title " . $barangid . " not found");
         }
         return view("barang/barangdetail", $data);
     }
     public function edit($barangid)
     {
-        $query = "SELECT * FROM tbm_barang WHERE barangid= '$barangid'";
-        dd($this->db->query($query)->getResultObject());
+        $query = "SELECT * FROM tbm_barang WHERE barangid=?";
         $data = [
             "title" => "edit barang",
-            "barang" => $this->db->query($query)->getResultObject()
+            "barang" => $this->db->query($query, $barangid)->getRow()
         ];
         return view("/barang/editbarang", $data);
     }
+    public function update($barangid)
+    {
+        $gambar = $this->request->getFile("file_gambar");
+        $barangcode = $this->request->getVar("barangcode");
+        $barangname = $this->request->getVar("barangname");
+        $stokawal = $this->request->getVar("stokawal");
+        $hargapp = $this->request->getVar("hargapp");
+        $hargabeli = $this->request->getVar("hargabeli");
+        $hargajual = $this->request->getVar("hargajual");
+        $satuan = $this->request->getVar("satuan");
+        $remarks = $this->request->getVar("remarks");
+        $ipAddress = $this->request->getIPAddress();
+        $inactive = $this->request->getPost("inactive");
+        $opedit = "admin";
+        $luedit = date("Y-m-d H:i:s");
+
+        $validation = $this->validate([
+            "barangname" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Nama barang tidak boleh kosong"
+                ]
+            ],
+            "stokawal" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Stok awal tidak boleh kosong"
+                ]
+            ],
+            "barangcode" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Kode barang tidak boleh kosong"
+                ]
+            ],
+            "hargabeli" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Harga beli tidak boleh kosong"
+                ]
+            ],
+            "hargajual" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Harga jual tidak boleh kosong"
+                ]
+            ],
+            "hargapp" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Harga app tidak boleh kosong"
+                ]
+            ],
+            "satuan" => [
+                "rules" => "required",
+                "errors" => [
+                    "required" => "Satuan tidak boleh kosong"
+                ]
+            ],
+            "file_gambar" => [
+                "rules" => "max_size[file_gambar, 2000]|ext_in[file_gambar,png,jpg]",
+                "errors" => [
+                    "max_size" => "Ukuran gambar produk harus kurang dari 2 MB",
+                    "ext_in" => "Format gambar harus png atau jpg"
+                ]
+            ]
+        ]);
+        if (!$validation) {
+            $validation = \Config\Services::validation();
+            $query = "SELECT * FROM tbm_barang WHERE barangid=?";
+            $data = [
+                "title" => "edit barang",
+                "validation" => $validation,
+                "barang" => $this->db->query($query, $barangid)->getRow()
+            ];
+            return view("barang/editbarang", $data);
+        } else {
+            $selectBarang = "SELECT * FROM tbm_barang WHERE barangid=?";
+            $barang = $this->db->query($selectBarang, $barangid)->getRow();
+            if ($gambar == "") {
+                if (empty($barang)) {
+                    throw new \CodeIgniter\Exceptions\PageNotFoundException("title " . $barangid . " not found");
+                } else {
+                    $query = "UPDATE tbm_barang SET barangcode=?, barangname=?, stokawal=?, hargapp=?,hargajual=?, hargabeli=?, satuan=?, remarks=?, pcedit=?, opedit=?, luedit=?, inactive=? WHERE barangid=?";
+                    $this->db->query($query, [$barangcode, $barangname, $stokawal, $hargapp, $hargajual, $hargabeli, $satuan, $remarks, $ipAddress, $opedit, $luedit, $inactive, $barangid]);
+                    session()->setFlashdata("success", "Update data barang berhasil");
+                    return redirect()->to("/barang");
+                }
+            } else {
+                unlink("assets/gambar-produk/" . $barang->file_gambar);
+                $gambar->move("assets/gambar-produk", $barang->barangid . "." . $gambar->getExtension());
+                $file_gambar = $gambar->getName();
+                $query = "UPDATE tbm_barang SET file_gambar=?, barangcode=?, barangname=?, stokawal=?, hargapp=?,hargajual=?, hargabeli=?, satuan=?, remarks=?, pcedit=?, opedit=?, luedit=?, inactive=? WHERE barangid=?";
+                $this->db->query($query, [$file_gambar, $barangcode, $barangname, $stokawal, $hargapp, $hargajual, $hargabeli, $satuan, $remarks, $ipAddress, $opedit, $luedit, $inactive, $barangid]);
+                session()->setFlashdata("success", "Update data barang berhasil");
+                return redirect()->to("/barang");
+            }
+        }
+    }
     public function delete($barangid)
     {
-        $query = "UPDATE tbm_barang SET dlt = 't' WHERE barangid='$barangid'";
-        $this->db->query($query);
+        $query = "UPDATE tbm_barang SET dlt=? WHERE barangid=?";
+        $this->db->query($query, ['t', $barangid]);
         session()->setFlashdata("success", "Barang berhasil dihapus");
         return redirect()->to("/barang");
     }
